4f4c6ed155a77f02bf2293580eb4c647f31841eb
v-allen17 <vca2102@columbia.edu>
2025-02-05 21:20:33 -0500

    Update and rename OddEven (1).java to OddEven.java

---
+++ Odd and Even Game/OddEven.java
@@ -0,0 +1,11 @@
+// OddEven.java
+// A tester for the game
+// Victoria Allen
+
+public class OddEven{
+    public static void main(String[] args){
+        Game g = new Game();
+        g.playSession();
+    
+    }
+}
